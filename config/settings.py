65791c08a60ac18e028e3a30b3a88a2ac8730402
akoshDev3212 <akbarbaxshillayev27@gmail.com>
2026-05-31 22:01:29 +0500

    Set DEBUG=False and use environment variable for SECRET_KEY

--- config/settings.py
+++ config/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-f0++a7-_*&l&yl_nl&)s=c8xqojc@x6d4+ljs3vgswep)14)8*')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['onlineshop-ism1.onrender.com', '127.0.0.1', 'localhost']
 
